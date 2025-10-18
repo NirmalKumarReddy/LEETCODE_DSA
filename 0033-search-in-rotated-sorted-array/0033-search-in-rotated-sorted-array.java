3363bb2becf90732738c9d6c3337e0fcdc2b79c6
Nirmal Reddy <63366332+NirmalKumarReddy@users.noreply.github.com>
2025-10-18 00:09:55 -0500

    Time: 0 ms (100%), Space: 41.8 MB (93.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-       int left = 0, right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
 
-            if (nums[mid] == target) return mid; // Found the target
-            
-            // Check which half is sorted
-            if (nums[left] <= nums[mid]) {  // Left half is sorted
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1; // Search in left half
-                } else {
-                    left = mid + 1;  // Search in right half
-                }
-            } else {  // Right half is sorted
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;  // Search in right half
-                } else {
-                    right = mid - 1; // Search in left half
+            int mid=l+(r-l)/2;
+
+                if(nums[mid]== target){
+                    return mid;
+             }
+
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && target<=nums[mid]){
+                      r=mid-1;
+                 }
+                else{
+                    l=mid+1;
                 }
             }
+            else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+
+            }
+           
+
         }
-        
-        return -1;  
+        return -1;
     }
 }
