e62e64493f981850822fa7ffb7078fdbcb3b06ff
Nirmal Reddy <63366332+NirmalKumarReddy@users.noreply.github.com>
2025-10-23 11:36:21 -0500

    Time: 2 ms (99.04%), Space: 45.1 MB (32.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,33 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    
-    Map<Integer,Integer> map = new HashMap<>();
-    int n = nums.length;
-    
-    
-    for(int i=0;i<n;i++){
-    int num=nums[i];
-    int extraNeeded= target - num;
-    if(map.containsKey(extraNeeded)){
-        return new int[] {map.get(extraNeeded),i};
-    }
-    map.put(num,i);
-
-    }
-    return new int[] {-1, -1};
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int rem = target - nums[i];
+            if(hm.containsKey(rem)){
+                return new int[]{hm.get(rem),i};
+            }
+            hm.put(nums[i],i);
+        }
+    return new int[]{};
     }
 }
-
-
-// bruteforce
-
-    //     for(int i=0;i<nums.length;i++){
-    //         for(int j=i+1;j<nums.length;j++){
-    //             if(nums[i]+nums[j]==target){
-    //                 return new int[] {i,j};
-    //             }
-    //         }
-    //     }
-    //   return new int[] {};
-
-// 2 pointer approch but i got wrong 3rd test case passsed i need to correct it 
