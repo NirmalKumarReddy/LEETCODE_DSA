39fbde2310a675994c0d6e2078f328daa7378d58
Nirmal Reddy <63366332+NirmalKumarReddy@users.noreply.github.com>
2025-10-28 23:30:57 -0500

    Time: 0 ms (100%), Space: 45.3 MB (73.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,21 @@
-class Solution {
+public class Solution {
     public int missingNumber(int[] nums) {
-       int range = nums.length;
-       int AS; int CS=0; int n;
-       AS = (range * (range+1))/2;
-
-       for(int i=0;i<range;i++){
-         CS+=nums[i];
-       }
-        n=AS-CS;
-        return n;
+        int xor = 0;
+        int n = nums.length;
+        // XOR all indices 0..n and all values in array
+        for (int i = 0; i < n; i++) {
+            xor ^= i;        // xor with index
+            xor ^= nums[i];  // xor with value
+        }
+        xor ^= n; // include n because indices run 0..n
+        return xor;
     }
-    
+
+    // quick test
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.missingNumber(new int[]{3,0,1})); // prints 2
+        System.out.println(s.missingNumber(new int[]{0,1}));   // prints 2
+        System.out.println(s.missingNumber(new int[]{9,6,4,2,3,5,7,0,1})); // prints 8
+    }
 }
