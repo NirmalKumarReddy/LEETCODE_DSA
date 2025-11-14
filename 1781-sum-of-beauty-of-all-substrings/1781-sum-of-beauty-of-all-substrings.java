479adc4828ba90810de8324da3336d8b74816e59
Nirmal Reddy <63366332+NirmalKumarReddy@users.noreply.github.com>
2025-11-13 23:07:13 -0600

    Time: 53 ms (95.63%), Space: 43.9 MB (37.38%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+            int maxFreq = 0;
+
+            for (int j = i; j < n; j++) {
+                int idx = s.charAt(j) - 'a';
+                freq[idx]++;
+                maxFreq = Math.max(maxFreq, freq[idx]);
+
+                // find min frequency > 0
+                int minFreq = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        minFreq = Math.min(minFreq, freq[k]);
+                    }
+                }
+
+                ans += (maxFreq - minFreq);
+            }
+        }
+
+        return ans;
+    }
+}
